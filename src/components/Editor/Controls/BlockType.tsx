6e4fc1f73db75b0e04a2e65deaf42f5d0681984d
mateja <mateja176@gmail.com>
2019-12-07 20:19:21 +0100

    refactor(Editor/Controls/BlockType): uses custom select component

--- src/components/Editor/Controls/BlockType.tsx
+++ src/components/Editor/Controls/BlockType.tsx
@@ -1,17 +1,25 @@
+import {
+  ClickAwayListener,
+  ListSubheader,
+  MenuItem,
+  Paper,
+  Popper,
+  Typography,
+} from '@material-ui/core';
+import { ArrowDropDown } from '@material-ui/icons';
 import { DraftBlockType, EditorState } from 'draft-js';
-import { find, pipe, prop } from 'ramda';
+import { find } from 'ramda';
 import React from 'react';
-import Select from 'react-select';
-import { useTheme } from '@material-ui/core';
+import { Box, Flex } from 'rebass';
 
-interface BlockValue {
+interface BlockTypeOption {
   label: string;
   value: DraftBlockType;
 }
 
-type BlockValues = BlockValue[];
+type BlockTypeOptions = BlockTypeOption[];
 
-const headings: BlockValues = [
+const headings: BlockTypeOptions = [
   { label: 'H1', value: 'header-one' },
   { label: 'H2', value: 'header-two' },
   { label: 'H3', value: 'header-three' },
@@ -20,25 +28,26 @@ const headings: BlockValues = [
   { label: 'H6', value: 'header-six' },
 ];
 
-const lists: BlockValues = [
+const lists: BlockTypeOptions = [
   { label: 'Bulleted', value: 'unordered-list-item' },
   { label: 'Ordered', value: 'ordered-list-item' },
 ];
 
-const otherBlockTypes: BlockValues = [
+const other: BlockTypeOptions = [
   { label: 'Blockquote', value: 'blockquote' },
   { label: 'Code Block', value: 'code-block' },
 ];
 
-const blockTypes = otherBlockTypes.concat(headings).concat(lists);
-
-const blockValues: Array<
-  BlockValue | { label: BlockValue['label']; options: BlockValues }
-> = [
-  ...otherBlockTypes,
-  { label: 'Lists', options: lists },
-  { label: 'Headings', options: headings },
+const blockTypeCategories: Array<{
+  category: string;
+  options: BlockTypeOptions;
+}> = [
+  { category: 'Lists', options: lists },
+  { category: 'Headings', options: headings },
+  { category: 'Other', options: other },
 ];
+
+const allOptions = blockTypeCategories.flatMap(({ options }) => options);
 
 export interface BlockTypeControlsProps {
   editorState: EditorState;
@@ -56,33 +65,68 @@ const BlocType: React.FC<BlockTypeControlsProps> = ({
     .getBlockForKey(selection.getStartKey())
     .getType();
 
-  const findActive = find<BlockValue>(({ value }) => blockType === value);
+  const findActive = find<BlockTypeOption>(({ value }) => blockType === value);
 
-  const activeType = findActive(blockTypes);
+  const activeType = findActive(allOptions);
 
-  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  const displayRef = React.useRef<HTMLDivElement>(null);
 
   return (
-    <Select
-      placeholder="Block Type..."
-      options={blockValues}
-      value={activeType || null}
-      onChange={pipe(
-        prop('value') as any,
-        onToggle,
-      )}
-      menuPortalTarget={document.body}
-      styles={{
-        container: base => ({
-          ...base,
-          minWidth: 150,
-        }),
-        menuPortal: base => ({
-          ...base,
-          zIndex: theme.zIndex.modal,
-        }),
+    <ClickAwayListener
+      onClickAway={() => {
+        if (open) {
+          toggleOpen();
+        }
       }}
-    />
+    >
+      <Box height="100%">
+        <Box
+          ref={displayRef}
+          onMouseDown={e => {
+            e.preventDefault();
+            toggleOpen();
+          }}
+          height="100%"
+        >
+          <MenuItem style={{ height: '100%', minWidth: 105 }}>
+            <Flex width="100%">
+              {(activeType && activeType.label) || (
+                <Typography color="textSecondary" style={{ flexGrow: 1 }}>B</Typography>
+              )}{' '}
+              {<ArrowDropDown />}
+            </Flex>
+          </MenuItem>
+        </Box>
+        <Popper open={open} anchorEl={displayRef.current}>
+          <Paper>
+            {blockTypeCategories.map(({ category, options }) => (
+              <Box key={category}>
+                <ListSubheader>{category}</ListSubheader>
+                {options.map(({ label, value }) => (
+                  <MenuItem
+                    key={value}
+                    value={value}
+                    onMouseDown={e => {
+                      e.preventDefault();
+
+                      onToggle(value);
+                    }}
+                  >
+                    {label}
+                  </MenuItem>
+                ))}
+              </Box>
+            ))}
+          </Paper>
+        </Popper>
+      </Box>
+    </ClickAwayListener>
   );
 };
 
