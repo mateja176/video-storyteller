cb2465ad3f10efa05a2e8d773dcaf7ed36d6627c
mateja <mateja176@gmail.com>
2019-12-09 23:34:02 +0100

    feat(Canvas/DevTools): adds missing ActionCreators types

--- src/pages/Canvas/DevTools.tsx
+++ src/pages/Canvas/DevTools.tsx
@@ -2,16 +2,15 @@ import { Button } from 'components';
 import { BlockStates } from 'models';
 import React from 'react';
 import { Box } from 'rebass';
-import { Dispatch } from 'redux';
+import { Action, Dispatch } from 'redux';
 import { createDevTools } from 'redux-devtools';
 // @ts-ignore
-import { ActionCreators } from 'redux-devtools-instrument';
+import { ActionCreators as InstrumentActionCreators } from 'redux-devtools-instrument';
+import { toObject } from 'utils';
 
 export interface MonitorState {}
 
-export interface Action {
-  type: string;
-}
+export type ActionId = number;
 
 export interface ActionById {
   type: string;
@@ -32,9 +31,9 @@ export interface ComputedState {
 export interface MonitorProps {
   monitorState: MonitorState;
   actionsById: ActionsById;
-  nextActionId: number;
-  stagedActionIds: number[];
-  skippedActionIds: number[];
+  nextActionId: ActionId;
+  stagedActionIds: ActionId[];
+  skippedActionIds: ActionId[];
   currentStateIndex: number;
   computedStates: ComputedState[];
   isLocked: boolean;
@@ -42,7 +41,99 @@ export interface MonitorProps {
   dispatch: Dispatch;
 }
 
-// @ts-ignore
+export const ActionCreators: IActionCreators = InstrumentActionCreators;
+
+export const actionTypes = [
+  'PERFORM_ACTION',
+  'RESET',
+  'ROLLBACK',
+  'COMMIT',
+  'SWEEP',
+  'TOGGLE_ACTION',
+  'SET_ACTIONS_ACTIVE',
+  'JUMP_TO_STATE',
+  'JUMP_TO_ACTION',
+  'REORDER_ACTION',
+  'IMPORT_STATE',
+  'LOCK_CHANGES',
+  'PAUSE_RECORDING',
+] as const;
+
+export const actionType = toObject(actionTypes);
+
+export type ActionType = typeof actionType;
+
+export interface IActionCreators {
+  performAction(
+    action: ActionById,
+    trace: ((action: ActionById) => ActionsById) | undefined,
+    traceLimit: number,
+    toExcludeFromTrace: () => void,
+  ):
+    | never
+    | {
+        type: ActionType['PERFORM_ACTION'];
+        action: ActionById;
+        timestamp: string;
+        stack: ActionsById;
+      };
+
+  reset(): { type: ActionType['RESET']; timestamp: string };
+
+  rollback(): { type: ActionType['ROLLBACK']; timestamp: string };
+
+  commit(): { type: ActionType['COMMIT']; timestamp: string };
+
+  sweep(): { type: ActionType['SWEEP'] };
+
+  toggleAction(id: number): { type: ActionType['TOGGLE_ACTION']; id: number };
+
+  setActionsActive(
+    start: number,
+    end: number,
+    active: boolean,
+  ): {
+    type: ActionType['SET_ACTIONS_ACTIVE'];
+    start: number;
+    end: number;
+    active: boolean;
+  };
+
+  reorderAction(
+    actionId: ActionId,
+    beforeActionId: ActionId,
+  ): {
+    type: ActionType['REORDER_ACTION'];
+    actionId: ActionId;
+    beforeActionId: ActionId;
+  };
+
+  jumpToState(
+    index: number,
+  ): { type: ActionType['JUMP_TO_STATE']; index: number };
+
+  jumpToAction(
+    actionId: ActionId,
+  ): { type: ActionType['JUMP_TO_ACTION']; actionId: ActionId };
+
+  importState(
+    nextLiftedState: MonitorProps['monitorState'],
+    noRecompute: boolean,
+  ): {
+    type: ActionType['IMPORT_STATE'];
+    nextLiftedState: MonitorProps['monitorState'];
+    noRecompute: boolean;
+  };
+
+  lockChanges(
+    status: boolean,
+  ): { type: ActionType['LOCK_CHANGES']; status: boolean };
+
+  pauseRecording(
+    status: boolean,
+  ): { type: ActionType['PAUSE_RECORDING']; status: boolean };
+}
+
 const StoryMonitor = (props: MonitorProps) => {
   const { dispatch } = props;
   console.log(props);
