aa2ca59255bfa58d96be1c2260ab1fed88436b3a
mateja <mateja176@gmail.com>
2019-12-10 00:13:21 +0100

    feat(Canvas/store): adds selectors and useSelector

--- src/pages/Canvas/store/index.ts
+++ src/pages/Canvas/store/index.ts
@@ -1,5 +1,9 @@
+import { useEffect } from '@storybook/addons';
+import { equals } from 'ramda';
+import { useState } from 'react';
 import { combineReducers, Reducer } from 'redux';
 import { configureStore } from 'redux-starter-kit';
+import { Selector } from 'reselect';
 import blockStates, { CudAction } from './blockStates';
 import scale, { ScaleAction } from './scale';
 
@@ -18,7 +22,28 @@ export type Action = CudAction | ScaleAction;
 
 const reducer: Reducer<State, Action> = combineReducers(actionReducerMap);
 
-export default configureStore({
+const store = configureStore({
   reducer,
   devTools: true,
 });
+
+export default store;
+
+export const selectBlockStates = (state: State) => state.blockStates;
+export const selectScale = (state: State) => state.scale;
+
+export const useSelector = <R>(selector: Selector<State, R>) => {
+  const [result, setResult] = useState(selector(store.getState()));
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      const newResult = selector(store.getState());
+      if (!equals(result, newResult)) {
+        setResult(newResult);
+      }
+    });
+    return unsubscribe;
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return result;
+};
