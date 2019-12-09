66725e9aa734b11a5a06372bcf5a0bb3576b8a79
mateja <mateja176@gmail.com>
2019-12-10 00:12:56 +0100

    feat(Canvas/store): adds ActionReducerMap to infer the type of State

--- src/pages/Canvas/store/index.ts
+++ src/pages/Canvas/store/index.ts
@@ -1,11 +1,22 @@
-import { combineReducers, configureStore } from 'redux-starter-kit';
-import blockStates from './blockStates';
-import scale from './scale';
+import { combineReducers, Reducer } from 'redux';
+import { configureStore } from 'redux-starter-kit';
+import blockStates, { CudAction } from './blockStates';
+import scale, { ScaleAction } from './scale';
 
-const reducer = combineReducers({
+const actionReducerMap = {
   blockStates,
   scale,
-});
+};
+
+type ActionReducerMap = typeof actionReducerMap;
+
+export type State = {
+  [key in keyof ActionReducerMap]: ReturnType<ActionReducerMap[key]>;
+};
+
+export type Action = CudAction | ScaleAction;
+
+const reducer: Reducer<State, Action> = combineReducers(actionReducerMap);
 
 export default configureStore({
   reducer,
