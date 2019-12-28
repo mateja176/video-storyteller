2115a0936629d57390da0629f49e7ea587676ddb
mateja <mateja176@gmail.com>
2019-12-28 13:28:14 +0100

    feat(Canvas/ActionCardForm): formats action type and adds titles

--- src/pages/Canvas/ActionCardForm.tsx
+++ src/pages/Canvas/ActionCardForm.tsx
@@ -1,7 +1,7 @@
 import { InputAdornment, TextField } from '@material-ui/core';
 import { Button } from 'components';
 import { Form, Formik } from 'formik';
-import { capitalize } from 'lodash';
+import { startCase } from 'lodash';
 import React from 'react';
 import { Flex } from 'rebass';
 import { ActionWithId, EditableAction } from './utils';
@@ -65,67 +65,76 @@ const ActionCardForm: React.FC<ActionCardFormProps> = ({
     enableReinitialize
     isInitialValid
   >
-    {({ isValid, handleChange, handleBlur, values, errors }) => (
-      <Flex flexDirection="column" px={2} pt={2} pb={1} flex={1}>
-        <Flex mb={3}>
-          <TextField
-            label="Action Id"
-            value={id}
-            variant="outlined"
-            disabled
-            style={{ marginRight: 5 }}
-            margin="dense"
-            type="number"
-          />
-          <TextField
-            label="Action Type"
-            value={capitalize(action.type)}
-            variant="outlined"
-            disabled
-            margin="dense"
-          />
-        </Flex>
-        <Form
-          onMouseEnter={() => {
-            setIsEditing(true);
-          }}
-          onMouseLeave={() => {
-            setIsEditing(false);
-          }}
-          style={{
-            flexGrow: 1,
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <TextField
-            name="timeDiff"
-            onBlur={handleBlur}
-            onChange={handleChange}
-            label="Time diff"
-            type="number"
-            variant="outlined"
-            value={values.timeDiff}
-            InputProps={{
-              endAdornment: <InputAdornment position="end">ms</InputAdornment>,
+    {({ isValid, handleChange, handleBlur, values, errors }) => {
+      const formattedActionType = startCase(action.type);
+
+      return (
+        <Flex flexDirection="column" px={2} pb={1} flex={1}>
+          <Flex mb={3}>
+            <TextField
+              label="Action Id"
+              value={id}
+              variant="outlined"
+              disabled
+              style={{ marginRight: 5, width: 90 }}
+              margin="dense"
+              type="number"
+              title={id.toString()}
+            />
+            <TextField
+              label="Action Type"
+              value={formattedActionType}
+              variant="outlined"
+              disabled
+              margin="dense"
+              title={formattedActionType}
+            />
+          </Flex>
+          <Form
+            onMouseEnter={() => {
+              setIsEditing(true);
             }}
-            error={Boolean(errors.timeDiff)}
-            helperText={errors.timeDiff}
-          />
-          {/* {isCfudAction(action as EditableAction) && (
+            onMouseLeave={() => {
+              setIsEditing(false);
+            }}
+            style={{
+              flexGrow: 1,
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <TextField
+              name="timeDiff"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              label="Time diff"
+              type="number"
+              variant="outlined"
+              value={values.timeDiff}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">ms</InputAdornment>
+                ),
+              }}
+              error={Boolean(errors.timeDiff)}
+              helperText={errors.timeDiff}
+              margin="dense"
+            />
+            {/* {isCfudAction(action as EditableAction) && (
           <Typography>
             Id: {(action as CfudAction).payload.id}
           </Typography>
         )} */}
-          <Flex mt="auto">
-            <Button type="submit" disabled={!isValid}>
-              Save edit
-            </Button>
-            <Button style={{ marginLeft: 'auto' }}>See more</Button>
-          </Flex>
-        </Form>
-      </Flex>
-    )}
+            <Flex mt="auto">
+              <Button type="submit" disabled={!isValid}>
+                Save edit
+              </Button>
+              <Button style={{ marginLeft: 'auto' }}>See more</Button>
+            </Flex>
+          </Form>
+        </Flex>
+      );
+    }}
   </Formik>
 );
 
