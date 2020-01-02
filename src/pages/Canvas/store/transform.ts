7d0638c1d84993b580c9b1f1e4eb1cbb694f2d46
mateja <mateja176@gmail.com>
2020-01-02 23:41:22 +0100

    fix(Canvas/store/transform): setScale.payload: TransformState

--- src/pages/Canvas/store/transform.ts
+++ src/pages/Canvas/store/transform.ts
@@ -21,7 +21,7 @@ export type SetTransformAction = ReturnType<typeof createSetTransform>;
 export const scaleSetType = 'transform/scale/set';
 export const createSetScale = createAction(
   scaleSetType,
-  action => (payload: Pick<TransformState, 'scale'>) => action(payload),
+  action => (payload: TransformState) => action(payload),
 );
 export type SetScaleAction = ReturnType<typeof createSetScale>;
 
@@ -40,7 +40,7 @@ export type TransformAction = SetTransformAction | ScaleAction | PositionAction;
 
 export default createReducer(initialState)<TransformAction>({
   [setTransformType]: (_, { payload }) => payload,
-  [scaleSetType]: (state, { payload }) => ({ ...state, ...payload }),
+  [scaleSetType]: (_, { payload }) => payload,
   [positionSetType]: (state, { payload }) => ({ ...state, ...payload }),
 });
 
