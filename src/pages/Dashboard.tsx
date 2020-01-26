54f0bd48814213ab113fe0b9bf8375d7df06313c
mateja <mateja176@gmail.com>
2020-01-26 14:20:32 +0100

    refactor(Dashboard): adds switch with fetchStoriesStatus cases

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -12,7 +12,7 @@ import { absoluteRootPaths } from 'Layout';
 import { add } from 'ramda';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Box } from 'rebass';
+import { Box, Flex } from 'rebass';
 import {
   createSetCurrentStoryId,
   selectCurrentStoryId,
@@ -41,52 +41,65 @@ const Dashboard: React.FC<DashboardProps> = () => {
     <Box>
       <Link to="canvas" />
       <Typography variant="h2">Recent stories</Typography>
-      <Box my={2}>
-        {fetchStoriesStatus === 'in progress' ? (
-          <Spinner />
-        ) : stories.length ? (
-          <List>
-            {stories.map(
-              ({ id, name, isPublic, durations, lastJumpedToActionId }) => {
-                const duration = durations.reduce(add, 0);
-                const time = new Date(duration);
-
-                const selected = currentStoryId === id;
-
+      <Flex my={2} flexDirection="column">
+        {(() => {
+          switch (fetchStoriesStatus) {
+            case 'in progress':
+              return <Spinner style={{ margin: 'auto' }} />;
+            case 'completed': {
+              if (stories.length) {
                 return (
-                  <ListItem
-                    key={id}
-                    button
-                    onClick={() => {
-                      setCurrentStoryId({ currentStoryId: id });
-                    }}
-                    selected={selected}
-                    disabled={selected}
-                  >
-                    <ListItemIcon>
-                      <Tooltip
-                        title={isPublic ? 'Public' : 'Draft'}
-                        placement="top"
-                      >
-                        {isPublic ? <Public /> : <Edit />}
-                      </Tooltip>
-                    </ListItemIcon>
-                    <ListItemText>
-                      <Link to={urlJoin(absoluteRootPaths.canvas, id)}>
-                        {name} ({time.getMinutes()}m {time.getSeconds()}s)
-                      </Link>
-                    </ListItemText>
-                  </ListItem>
+                  <List>
+                    {stories.map(({ id, name, isPublic, durations }) => {
+                      const duration = durations.reduce(add, 0);
+                      const time = new Date(duration);
+
+                      const selected = currentStoryId === id;
+
+                      return (
+                        <ListItem
+                          key={id}
+                          button
+                          onClick={() => {
+                            setCurrentStoryId({ currentStoryId: id });
+                          }}
+                          selected={selected}
+                          disabled={selected}
+                        >
+                          <ListItemIcon>
+                            <Tooltip
+                              title={isPublic ? 'Public' : 'Draft'}
+                              placement="top"
+                            >
+                              {isPublic ? <Public /> : <Edit />}
+                            </Tooltip>
+                          </ListItemIcon>
+                          <ListItemText>
+                            <Link to={urlJoin(absoluteRootPaths.canvas, id)}>
+                              {name} ({time.getMinutes()}m {time.getSeconds()}
+                              s)
+                            </Link>
+                          </ListItemText>
+                        </ListItem>
+                      );
+                    })}
+                  </List>
                 );
-              },
-            )}
-          </List>
-        ) : (
-          <Link to="canvas">
-            <i color={theme.palette.primary.main}>Create your first story</i>
-          </Link>
-        )}
-      </Box>
+              } else {
+                return (
+                  <Link to="canvas">
+                    <i color={theme.palette.primary.main}>
+                      Create your first story
+                    </i>
+                  </Link>
+                );
+              }
+            }
+            default:
+              return null;
+          }
+        })()}
+      </Flex>
     </Box>
   );
 };
