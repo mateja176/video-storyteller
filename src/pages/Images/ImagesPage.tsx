f19a5b54c4eec89451a09cd88a13f8e2fe66b24d
mateja <mateja176@gmail.com>
2020-01-28 19:36:36 +0100

    feat(Images): adds header

--- src/pages/Images/ImagesPage.tsx
+++ src/pages/Images/ImagesPage.tsx
@@ -1,15 +1,26 @@
+import { Typography } from '@material-ui/core';
 import { Switch } from 'components';
 import React, { FC } from 'react';
 import { Route, RouteComponentProps } from 'react-router-dom';
+import { Box, Flex } from 'rebass';
 import urlJoin from 'url-join';
 import Images from './Images';
 import Upload from './Upload';
+
+const ImagesWithHeader = () => (
+  <Box>
+    <Flex mb={2}>
+      <Typography variant="h2">Your Images</Typography>
+    </Flex>
+    <Images />
+  </Box>
+);
 
 export interface ImagesProps extends RouteComponentProps {}
 
 const ImagesPage: FC<ImagesProps> = ({ match: { path } }) => (
   <Switch>
-    <Route exact path={urlJoin(path, '/')} component={Images} />
+    <Route exact path={urlJoin(path, '/')} component={ImagesWithHeader} />
     <Route path={urlJoin(path, 'upload')} component={Upload} />
   </Switch>
 );
