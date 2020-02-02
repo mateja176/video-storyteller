bb32d591a6a9b52563eb7420358b01ea4f816d47
mateja <mateja176@gmail.com>
2020-02-02 20:58:37 +0100

    feat(models/components): adds WithEditorState

--- src/models/components.ts
+++ src/models/components.ts
@@ -1,7 +1,8 @@
 import { EditorState } from 'draft-js';
 
-export interface EditorProps {
-  editorState: EditorState;
+export type WithEditorState = { editorState: EditorState };
+
+export interface EditorProps extends WithEditorState {
   setEditorState: (editorState: EditorState) => void;
   onFocus?: React.FocusEventHandler;
   onBlur?: React.FocusEventHandler;
