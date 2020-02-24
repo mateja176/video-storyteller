1f4acd0eb85cdcddbaf5b5b5369d133db5e07365
mateja <mateja176@gmail.com>
2020-02-24 22:19:18 +0100

    feat(Canvas/Rnd): sets Rnd left and top while dragging

--- src/pages/Canvas/Rnd.tsx
+++ src/pages/Canvas/Rnd.tsx
@@ -1,6 +1,7 @@
+import rndDragPreview from 'assets/img/rnd-drag-preview.png';
 import { SimpleAction } from 'models';
 import React from 'react';
-import { DragSourceHookSpec, useDrag } from 'react-dnd';
+import { DragPreviewImage, DragSourceHookSpec, useDrag } from 'react-dnd';
 import { UpdateAction } from './store/blockStates';
 
 export type DragSourceSpec = DragSourceHookSpec<
@@ -9,12 +10,22 @@ export type DragSourceSpec = DragSourceHookSpec<
   void
 >;
 
+const initialClientCoords: Pick<React.MouseEvent, 'clientX' | 'clientY'> = {
+  clientX: 0,
+  clientY: 0,
+};
+
 export interface RndProps
   extends React.HTMLProps<HTMLDivElement>,
-    Pick<DragSourceSpec, 'begin' | 'end' | 'canDrag'> {}
+    Pick<DragSourceSpec, 'begin' | 'end' | 'canDrag'> {
+  style: React.CSSProperties & {
+    left: number;
+    top: number;
+  };
+}
 
-const Rnd: React.FC<RndProps> = ({ begin, end, canDrag, ...props }) => {
-  const [, dragRef] = useDrag({
+const Rnd: React.FC<RndProps> = ({ begin, end, canDrag, style, ...props }) => {
+  const [, dragRef, previewRef] = useDrag({
     item: {
       type: 'updateMove',
     },
@@ -23,7 +34,46 @@ const Rnd: React.FC<RndProps> = ({ begin, end, canDrag, ...props }) => {
     canDrag,
   });
 
-  return <div ref={dragRef} {...props} />;
+  const [startClientCoords, setStartClientCoords] = React.useState(
+    initialClientCoords,
+  );
+  const [deltaClientCoords, setDeltaClientCoords] = React.useState(
+    initialClientCoords,
+  );
+
+  const left = style.left + deltaClientCoords.clientX;
+  const top = style.top + deltaClientCoords.clientY;
+
+  return (
+    <>
+      <DragPreviewImage connect={previewRef} src={rndDragPreview} />
+      <div
+        ref={dragRef}
+        {...props}
+        style={{
+          ...style,
+          top,
+          left,
+        }}
+        onDragStart={({ clientX, clientY }) => {
+          setStartClientCoords({
+            clientX,
+            clientY,
+          });
+        }}
+        onDrag={({ clientX, clientY }) => {
+          setDeltaClientCoords({
+            clientX: clientX - startClientCoords.clientX,
+            clientY: clientY - startClientCoords.clientY,
+          });
+        }}
+        onDragEnd={() => {
+          setStartClientCoords(initialClientCoords);
+          setDeltaClientCoords(initialClientCoords);
+        }}
+      />
+    </>
+  );
 };
 
 export default Rnd;
