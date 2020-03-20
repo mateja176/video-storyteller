6d427a3f8aa2190749f607e12fc51409ecc816ed
mateja <mateja176@gmail.com>
2020-03-21 00:30:02 +0100

    feat(analytics): logs page_view

--- src/env.ts
+++ src/env.ts
@@ -23,6 +23,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_CONFIG_MEASUREMENT_ID,
 };
 
 assertArePropsDefined(getNoEnvVariableMessage, firebaseConfig);
--- src/services/history.ts
+++ src/services/history.ts
@@ -1,3 +1,9 @@
+import 'firebase/analytics';
 import { createBrowserHistory } from 'history';
+import { firebase } from './firebase';
 
 export const history = createBrowserHistory();
+
+history.listen(location =>
+  firebase.analytics().logEvent('page_view', location),
+);
