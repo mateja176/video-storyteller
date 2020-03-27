3e39eadad6e39bde81e2db70ab3676f329f8352d
mateja <ma.petrovic@levi9.com>
2020-03-28 00:30:39 +0100

    feat(functions): sets up dotenv

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv';
 import * as functions from 'firebase-functions';
+
+dotenv.config();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
