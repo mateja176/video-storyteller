1cdc1eb87a64270e5638bf338deb8783a8208113
mateja <ma.petrovic@levi9.com>
2020-03-28 01:26:45 +0100

    feat(functions): creates a request to get back a jwt
    and sends to jwt to the client

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,28 @@
 import * as dotenv from 'dotenv';
 import * as functions from 'firebase-functions';
+import * as request from 'request';
 
 dotenv.config();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+export const iconfinderToken = functions.https.onRequest((req, res) => {
+  request.post(
+    'https://www.iconfinder.com/api/v3/oauth2/token',
+    {
+      form: {
+        grant_type: 'jwt_bearer',
+        client_id: process.env.ICONFINDER_CLIENT_ID,
+        client_secret: process.env.ICONFINDER_CLIENT_SECRET,
+      },
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        res.send(body);
+      } else {
+        console.log(error, response, body);
+      }
+    },
+  );
 });
