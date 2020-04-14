8c960ab3b49f6433edf6e931aa612ea2015f838a
mateja <mateja176@gmail.com>
2020-04-14 22:08:41 +0200

    feat(Canvas/ActionCardForm): migrates to useFormik

--- src/pages/Canvas/ActionCardForm.tsx
+++ src/pages/Canvas/ActionCardForm.tsx
@@ -9,7 +9,7 @@ import {
 import clsx from 'clsx';
 import { Button } from 'components';
 import { convertFromRaw } from 'draft-js';
-import { Form, Formik } from 'formik';
+import { useFormik } from 'formik';
 import { useFlicker } from 'hooks';
 import { equals } from 'ramda';
 import React from 'react';
@@ -36,6 +36,8 @@ const constantTextFieldProps = {
   },
 } as const;
 
+const px = <InputAdornment position="end">px</InputAdornment>;
+
 interface InitialValues {
   duration: number;
 }
@@ -49,14 +51,14 @@ interface Values
 }
 
 export interface ActionCardFormProps extends Pick<ICanvasContext, 'isAuthor'> {
-  handleSubmit: (values: Values) => void;
+  onSubmit: (values: Values) => void;
   initialValues: InitialValues;
   action: Action;
 }
 
 const ActionCardForm: React.FC<ActionCardFormProps> = ({
   isAuthor,
-  handleSubmit,
+  onSubmit,
   initialValues,
   action,
 }) => {
@@ -102,232 +104,222 @@ const ActionCardForm: React.FC<ActionCardFormProps> = ({
       : '',
   };
 
-  const px = <InputAdornment position="end">px</InputAdornment>;
+  const {
+    handleSubmit,
+    isValid,
+    handleChange,
+    handleBlur,
+    values,
+    errors,
+    resetForm,
+    submitForm,
+  } = useFormik({
+    initialValues: formattedInitialValues,
+    onSubmit,
+    enableReinitialize: true,
+    validate: formValues => {
+      if (formValues.duration < -1) {
+        return {
+          duration: 'Difference must be a positive number',
+        };
+      } else {
+        return undefined;
+      }
+    },
+  });
 
-  const flickerClasses = useFlicker({ disabled: false });
+  const areValuesEqual = equals(formattedInitialValues, values);
+
+  const saveDisabled = !isValid || areValuesEqual;
+
+  const flickerClasses = useFlicker({ disabled: saveDisabled });
 
   return (
-    <Formik
-      initialValues={formattedInitialValues}
-      onSubmit={handleSubmit}
-      validate={values => {
-        if (values.duration < -1) {
-          return {
-            duration: 'Difference must be a positive number',
-          };
-        } else {
-          return undefined;
+    <ClickAwayListener
+      onClickAway={() => {
+        if (!areValuesEqual) {
+          submitForm();
         }
       }}
-      enableReinitialize
-      isInitialValid
     >
-      {({
-        isValid,
-        handleChange,
-        handleBlur,
-        values,
-        errors,
-        resetForm,
-        submitForm,
-      }) => {
-        // const formattedActionType = startCase(action.type);
-
-        const areValuesEqual = equals(formattedInitialValues, values);
-        const saveDisabled = !isValid || areValuesEqual;
-
-        return (
-          <ClickAwayListener
-            onClickAway={() => {
-              if (!areValuesEqual) {
-                submitForm();
-              }
+      <Box display="flex" flexDirection="column" px={2} flex={1}>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <TextField
+            {...textFieldProps}
+            name="duration"
+            value={values.duration}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            label="Duration"
+            type="number"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">ms</InputAdornment>,
             }}
-          >
-            <Box display="flex" flexDirection="column" px={2} flex={1}>
-              <Form
-                style={{
-                  flexGrow: 1,
-                  display: 'flex',
-                  flexDirection: 'column',
+            error={Boolean(errors.duration)}
+            helperText={errors.duration}
+          />
+          {isUpdateResizeAction(action) && (
+            <Box display="flex">
+              <TextField
+                name="width"
+                label="Width"
+                value={values.width}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">px</InputAdornment>
+                  ),
+                }}
+                style={{ marginRight: 5 }}
+              />
+              <TextField
+                name="height"
+                label="Height"
+                value={values.height}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">px</InputAdornment>
+                  ),
+                }}
+              />
+            </Box>
+          )}
+          {(isScaleAction(action) || isSetTransformAction(action)) && (
+            <TextField
+              {...textFieldProps}
+              type="number"
+              name="scale"
+              label="Zoom"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.scale}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+            />
+          )}
+          {isUpdateEditAction(action) && (
+            <TextField
+              {...textFieldProps}
+              type="text"
+              name="editorState"
+              label="Editor State"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.editorState}
+              multiline
+              rows={2}
+              rowsMax={2}
+              disabled
+            />
+          )}
+          {isUpdateRenameImageAction(action) && (
+            <TextField
+              {...textFieldProps}
+              type="text"
+              name="name"
+              label="Image Name"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.name}
+              multiline
+              rows={3}
+              rowsMax={3}
+              disabled
+            />
+          )}
+          {isUpdateMoveAction(action) && (
+            <Box display="flex">
+              <TextField
+                {...textFieldProps}
+                type="number"
+                name="left"
+                label="Left"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.left}
+                style={{ marginRight: 5 }}
+                InputProps={{
+                  endAdornment: px,
+                }}
+              />
+              <TextField
+                {...textFieldProps}
+                type="number"
+                name="top"
+                label="Top"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.top}
+                InputProps={{
+                  endAdornment: px,
+                }}
+              />
+            </Box>
+          )}
+          {(isPositionAction(action) || isSetTransformAction(action)) && (
+            <Box display="flex">
+              <TextField
+                {...textFieldProps}
+                type="number"
+                name="x"
+                label="X Coordinate"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.x}
+                style={{ marginRight: 5 }}
+                InputProps={{
+                  endAdornment: px,
+                }}
+              />
+              <TextField
+                {...textFieldProps}
+                type="number"
+                name="y"
+                label="Y Coordinate"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.y}
+                InputProps={{
+                  endAdornment: px,
+                }}
+              />
+            </Box>
+          )}
+          {isAuthor && (
+            <Box display="flex" mt="auto">
+              <Button
+                className={clsx(Object.values(flickerClasses))}
+                size="small"
+                type="submit"
+                disabled={saveDisabled}
+              >
+                Save edit
+              </Button>
+              <Button
+                disabled={areValuesEqual}
+                size="small"
+                onClick={() => {
+                  resetForm();
                 }}
               >
-                <TextField
-                  {...textFieldProps}
-                  name="duration"
-                  value={values.duration}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  label="Duration"
-                  type="number"
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">ms</InputAdornment>
-                    ),
-                  }}
-                  error={Boolean(errors.duration)}
-                  helperText={errors.duration}
-                />
-                {isUpdateResizeAction(action) && (
-                  <Box display="flex">
-                    <TextField
-                      name="width"
-                      label="Width"
-                      value={values.width}
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">px</InputAdornment>
-                        ),
-                      }}
-                      style={{ marginRight: 5 }}
-                    />
-                    <TextField
-                      name="height"
-                      label="Height"
-                      value={values.height}
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">px</InputAdornment>
-                        ),
-                      }}
-                    />
-                  </Box>
-                )}
-                {(isScaleAction(action) || isSetTransformAction(action)) && (
-                  <TextField
-                    {...textFieldProps}
-                    type="number"
-                    name="scale"
-                    label="Zoom"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.scale}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">%</InputAdornment>
-                      ),
-                    }}
-                  />
-                )}
-                {isUpdateEditAction(action) && (
-                  <TextField
-                    {...textFieldProps}
-                    type="text"
-                    name="editorState"
-                    label="Editor State"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.editorState}
-                    multiline
-                    rows={2}
-                    rowsMax={2}
-                    disabled
-                  />
-                )}
-                {isUpdateRenameImageAction(action) && (
-                  <TextField
-                    {...textFieldProps}
-                    type="text"
-                    name="name"
-                    label="Image Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.name}
-                    multiline
-                    rows={3}
-                    rowsMax={3}
-                    disabled
-                  />
-                )}
-                {isUpdateMoveAction(action) && (
-                  <Box display="flex">
-                    <TextField
-                      {...textFieldProps}
-                      type="number"
-                      name="left"
-                      label="Left"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.left}
-                      style={{ marginRight: 5 }}
-                      InputProps={{
-                        endAdornment: px,
-                      }}
-                    />
-                    <TextField
-                      {...textFieldProps}
-                      type="number"
-                      name="top"
-                      label="Top"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.top}
-                      InputProps={{
-                        endAdornment: px,
-                      }}
-                    />
-                  </Box>
-                )}
-                {(isPositionAction(action) || isSetTransformAction(action)) && (
-                  <Box display="flex">
-                    <TextField
-                      {...textFieldProps}
-                      type="number"
-                      name="x"
-                      label="X Coordinate"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.x}
-                      style={{ marginRight: 5 }}
-                      InputProps={{
-                        endAdornment: px,
-                      }}
-                    />
-                    <TextField
-                      {...textFieldProps}
-                      type="number"
-                      name="y"
-                      label="Y Coordinate"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.y}
-                      InputProps={{
-                        endAdornment: px,
-                      }}
-                    />
-                  </Box>
-                )}
-                {isAuthor && (
-                  <Box display="flex" mt="auto">
-                    <Button
-                      className={clsx(Object.values(flickerClasses))}
-                      size="small"
-                      type="submit"
-                      disabled={saveDisabled}
-                    >
-                      Save edit
-                    </Button>
-                    <Button
-                      disabled={areValuesEqual}
-                      size="small"
-                      onClick={() => {
-                        resetForm();
-                      }}
-                    >
-                      Cancel
-                    </Button>
-                  </Box>
-                )}
-              </Form>
+                Cancel
+              </Button>
             </Box>
-          </ClickAwayListener>
-        );
-      }}
-    </Formik>
+          )}
+        </form>
+      </Box>
+    </ClickAwayListener>
   );
 };
 
